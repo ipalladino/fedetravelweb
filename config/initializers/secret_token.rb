ef8b17f105e99b58a0c847fdbe5b4b51fdd90ca8
Ignacio Palladino <ignacio@flingo.tv>
2012-08-14 17:39:00 -0700

    commiting full project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Travelweb::Application.config.secret_token = '9d868cdef817b9bcd6446c42124e1f0d4ff658e1ad06b2a6caf901cc08d5f45821ac5eaf57a1690772ce0dca4ff72bb67b35c728843d6537aa214bea66ee1714'
