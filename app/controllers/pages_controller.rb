575cdc46351e69986c4f4b387fdb832613e1545a
Ignacio Palladino <ignacio@flingo.tv>
2012-09-06 00:43:35 -0700

    Updated all styles and views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,7 @@
 class PagesController < ApplicationController
   
   def index
-    if admin?
-      redirect_to :action => "home_admin"
-    elsif user?
+    if admin? || user?
       redirect_to :action => "home"
     else
       #redirect_to :action => "login"
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,21 +2,36 @@ module ApplicationHelper
   def make_menu
     if(admin?)
       link = link_to 'Sign out', :controller => 'sessions', :action => 'destroy', :method=>:delete
-      html = "<span id='nav-top'>" + link + "  |  <a href='/posts/new'>New Post</a>  |  <a href='/users/'>Users</a>  |  <a href='/posts'>Posts</a></span>"
+      html = "<div id='nav-top'>" + link + "  |  <a href='/posts/new'>New Post</a>  |  <a href='/users/'>Users</a>  |  <a href='/posts'>Posts</a></div>"
       render(:inline => html)
     elsif(user?)
       link = link_to 'Sign out', :controller => 'sessions', :action => 'destroy', :method=>:delete
-      html = "<span  id='nav-top'>" + link + "</span>"
+      html = "<div  id='nav-top'>" + link + "</div>"
       render(:inline => html)
     end
   end
   def showPost(counter = 0)
     @isAdmin = admin?
     @counter = counter
-    render '/posts/single_item'
+    render '/posts/img_vid_map'
   end
   
   def make_menu_page
     render '/layouts/menu'
+  end
+  
+  def isSelected?(type)
+    puts "comparing:"
+    puts Pathname.new(request.fullpath).basename
+    puts type
+    if(String(Pathname.new(request.fullpath).basename) == type)
+      puts "TRUE"
+      #html = "class=\"selected\""
+      html = "style=\"color:#ff9900;\""
+      render(:inline => html) 
+    else
+      puts "FALSE"
+      return ""
+    end
   end
 end
